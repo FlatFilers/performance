5ac1ad8a85df0b0ddaa5d48d612b821f58fe8449
Eric Crane <eric.crane10@gmail.com>
2023-11-09 16:38:58 -0800

    loyal json app

--- index.ts
+++ index.ts
@@ -1,19 +1,12 @@
 /**
  * See all code examples: https://github.com/FlatFilers/flatfile-docs-kitchen-sink
  */
-import { FlatfileRecord } from "@flatfile/hooks";
-import { recordHook } from "@flatfile/plugin-record-hook";
-import { FlatfileEvent, Client } from "@flatfile/listener";
+import { Client } from "@flatfile/listener";
 import api from "@flatfile/api";
 import { JobResponse } from "@flatfile/api/api";
-import axios from "axios";
-import { blueprint } from "./blueprint";
-import { formatRecordDates } from "./dateFormatting";
-import { RecordsResponse } from "@flatfile/api/api";
 import { ZipExtractor } from "@flatfile/plugin-zip-extractor";
 import { ExcelExtractor } from "@flatfile/plugin-xlsx-extractor";
 import { JSONExtractor } from "@flatfile/plugin-json-extractor";
-
 
 export default function flatfileEventListener(listener: Client) {
 
@@ -38,34 +31,37 @@ export default function flatfileEventListener(listener: Client) {
       })
 
       const timestamp = new Date();
-      // CREATE WORKBOOK FROM BLUEPRINT
-      const createWorkbook = await api.workbooks.create({
-        spaceId: spaceId,
-        environmentId: environmentId,
-        labels: ['primary'],
-        name: `${timestamp} Load Test Wide`,
-        sheets: blueprint,
-        actions: [
-          {
-            operation: 'submitAction',
-            mode: 'foreground',
-            label: 'Submit',
-            description: 'Send a webhook to the app',
-            primary: true,
-          },
-        ],
-      })
-
-      const workbookId = createWorkbook.data?.id
 
       // ADD WORKBOOK TO SPACE
-      if (workbookId) {
-        // Need to refresh until update to Spaces to poll for changes
         const updatedSpace = await api.spaces.update(spaceId, {
           environmentId: environmentId,
-          primaryWorkbookId: workbookId,
+          metadata: {
+            theme: {
+              root: {
+                primaryColor: "#00263E",
+              },
+              sidebar: {
+                logo: "https://media.licdn.com/dms/image/C4D0BAQFi757SbjmWrQ/company-logo_200_200/0/1646169798107/loyal_health_logo?e=1707955200&v=beta&t=s0C4u-O-yVS3zpBNmcJCG6qPurynwJYMCQExnodx4mY",
+              },
+            },
+            sidebarConfig: {
+              showDataChecklist: false,
+            },
+          }
         })
-      }
+
+      const welcomepage = await api.documents.create(spaceId,{
+        title: 'How to use this app',
+        body:
+        '# Welcome\n' +
+        '![Loyal logo](https://media.licdn.com/dms/image/C4D0BAQFi757SbjmWrQ/company-logo_200_200/0/1646169798107/loyal_health_logo?e=1707955200&v=beta&t=s0C4u-O-yVS3zpBNmcJCG6qPurynwJYMCQExnodx4mY)\n' +
+        "## We built this quick and simple app for you to transform JSON into CSV files.\n" +
+        "Simply navigate to the Files section on the left side bar.\n" +
+        'Upload your JSON file.\n' +
+        'Then you should see the file appear in the "Available downloads" subheader within files.\n' +
+        'Let us know how we can help extend this further! Mike Denenberg, mike.d@flatfile.io.\n' +
+        '---\n',
+      })
 
       await api.jobs.complete(jobId, {
         info: 'This space is completed.',
@@ -84,104 +80,6 @@ export default function flatfileEventListener(listener: Client) {
   listener.use(ExcelExtractor());
 
   // JSON FILE SUPPORT
-  // Initialize the JSON extractor
-  const jsonExtractor = JSONExtractor();
+  listener.use(JSONExtractor());
 
-  // Register the extractor as a middleware for the Flatfile listener
-  listener.use(jsonExtractor);
-
-  // DYNAMIC VALIDATIONS WITH THE RECORD HOOK PLUGIN
-  listener.use(
-    // bulkRecordHook(
-    recordHook(
-      "orders",
-      (record: FlatfileRecord) => {
-        // return records.map((record) => {
-          // transforms all First Names to lowercase
-          const value = record.get("first_name");
-          if (typeof value === "string") {
-            record.set("first_name", value.toLowerCase());
-          }
-
-          // // validates all email addresses in the sheet and returns back an error message against them
-          // const email = record.get("email") as string;
-          // const validEmailAddress = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-          // if (!validEmailAddress.test(email)) {
-          //   console.log("Invalid email address");
-          //   record.addError("email", "Invalid email address");
-          // }
-
-          // // formats all dates to a standard structure
-          // try {
-          //   formatRecordDates(record, "orders")
-          // } catch (error) {
-          //   // console.log('Error occurred during date formatting:', error)
-          // }
-
-          return record;
-        // });
-      }
-      ),
-      // { chunkSize: 10000, parallel: 10 }
-    ),
-
-  // SUBMIT ALL DATA IN THE WORKBOOK TO A WEBHOOK; THIS PAGES THROUGH ALL RECORDS AND BUILDS A RESPONSE CONTAINING ALL DATA
-  listener.filter({ job: "workbook:submitAction" }, (configure) => {
-    configure.on(
-      "job:ready",
-      async ({ context: { jobId, workbookId }, payload }: FlatfileEvent) => {
-        const sheets = await api.sheets.list({ workbookId });
-        let records: RecordsResponse;
-        const webhookReceiver =
-          process.env.WEBHOOK_SITE_URL ||
-          "https://webhook.site/0aff6339-9c0b-4d6c-96ba-16ccf7b3089b"; //update this if you would like to test yourself
-
-        try {
-          await api.jobs.ack(jobId, {
-            info: "Starting job to submit action to webhook.site",
-            progress: 10,
-          });
-
-          for (const [index, sheet] of sheets.data.entries()) {
-            const recordCount = await api.sheets.getRecordCounts(sheet.id);
-            const pages = Math.ceil(recordCount.data.counts.total / 1000);
-            const response = await axios.post(
-              webhookReceiver,
-              {
-                data: {
-                  "sheetId": sheet.id,
-                  "workbookId": workbookId,
-                  "pages": pages,
-                  "recordcount": recordCount,
-                }
-              },
-              {
-                headers: {
-                  "Content-Type": "application/json",
-                },
-              }
-            );
-          }
-
-          await api.jobs.complete(jobId, {
-            outcome: {
-              message:
-                "Data was successfully submitted to webhook.site. Go check it out at " +
-                webhookReceiver +
-                ".",
-            },
-          });
-
-        } catch (error) {
-          console.log(`webhook.site[error]: ${JSON.stringify(error, null, 2)}`);
-          await api.jobs.fail(jobId, {
-            outcome: {
-              message:
-                "This job failed probably because it couldn't find the webhook.site URL.",
-            },
-          });
-        }
-      }
-    );
-  });
 }
